b1e2570c1dde2d111c6ef1bfb5fcc19e27d4a328
Suzie Q <suzieqwolf@gmail.com>
2020-05-28 14:00:57 -0600

    Adds class Activity to activity.js w/ constructor.

--- src/activity.js
+++ src/activity.js
@@ -1,1 +1,10 @@
-
+class Activity {
+    constructor(category, description, minutes, seconds) {
+        this.category = category;
+        this.description = description;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.isCompleted = false;
+        this.id = Date.now();
+    }
+}
