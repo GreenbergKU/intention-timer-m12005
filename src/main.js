5684f0e629b632323e5379a0a1c2887c6bda5f67
Joshua Sevy <joshuasevy@outlook.com>
2020-05-29 18:52:22 -0600

    Add functions for task validation and error message

--- src/main.js
+++ src/main.js
@@ -5,82 +5,115 @@ var studButton = document.querySelector('#stud-button');
 var medButton = document.querySelector('#med-button');
 var exButton = document.querySelector('#ex-button');
 var startButton = document.querySelector('#start-activity-button');
+
 var currentActivity;
+var savedActivity = [];
+
+// categorySection.addEventListener('click', function(event) {
+//   studButton.isActive = event.target.id === studButton.id;
+//   exButton.isActive = event.target.id === exButton.id;
+//   medButton.isActive = event.target.id === medButton.id;
+
+//   if (studButton.isActive === true) {
+//     studButton.childNodes[1].src = `./assets/${studButton.name}-active.svg`;
+//     studButton.classList.add('stud-color');
+//     startButton.disabled = false;
+//   } else {
+//     studButton.childNodes[1].src = `./assets/${studButton.name}.svg`;
+//     studButton.classList.remove('stud-color');
+//   };
+//
+//   if (exButton.isActive === true) {
+//     exButton.childNodes[1].src = `./assets/${exButton.name}-active.svg`;
+//     exButton.classList.add('ex-color');
+//     startButton.disabled = false;
+//   } else {
+//     exButton.childNodes[1].src = `./assets/${exButton.name}.svg`;
+//     exButton.classList.remove('ex-color');
+//   };
+//
+//   if (medButton.isActive === true) {
+//     medButton.childNodes[1].src = `./assets/${medButton.name}-active.svg`;
+//     medButton.classList.add('med-color');
+//     startButton.disabled = false;
+//   } else {
+//     medButton.childNodes[1].src = `./assets/${medButton.name}.svg`;
+//     medButton.classList.remove('med-color');
+//     console.log(event.target);
+//   };
+// });
+
 
 categorySection.addEventListener('click', function(event) {
 
+    for (var i = 1; i < categorySection.children.length; i++) {
+        var categoryChild = categorySection.children[i];
+        if (categoryChild.id === event.target.id) {
+            categoryChild.isActive = true;
+            categoryChild.childNodes[1].src = `./assets/${categoryChild.name}-active.svg`;
+            categoryChild.classList.add(`${categoryChild.name}-color`);
+        } else {
+            categoryChild.isActive = false;
+            categoryChild.childNodes[1].src = `./assets/${categoryChild.name}.svg`;
+            categoryChild.classList.remove(`${categoryChild.name}-color`);
+        };
+                console.log(
+                    "i=", i,
+                    "categoryChild.isActive=", categoryChild.isActive,
+                    "event.target.name=", event.target.name,
+                );
+}});
 
-  studButton.isActive = event.target.id === studButton.id;
-  exButton.isActive = event.target.id === exButton.id;
-  medButton.isActive = event.target.id === medButton.id;
+// newActivitySection.addEventListener('click', function(event) {
+//   // function for that start activity button
+//   if(event.target.id === startButton.id) {
+//     var selectedCategory = 'Study';
+//     var descriptionInput = document.querySelector('#task-input').value;
+//     var minuteInput = document.querySelector('#minute-input').value;
+//     var secondInput = document.querySelector('#second-input').value;
+//     var currentActivity = new Activity(selectedCategory, descriptionInput, minuteInput, secondInput);
+//     console.log(event.target);
+//   }
+// });
 
+//Event Handlers
 
-  if (studButton.isActive === true) {
-    studButton.childNodes[1].src = `./assets/${studButton.name}-active.svg`;
-    studButton.classList.add('stud-color');
-    startButton.disabled = false;
-  } else {
-    studButton.childNodes[1].src = `./assets/${studButton.name}.svg`;
-    studButton.classList.remove('stud-color');
-  };
+var form = document.getElementById('form');
+var task = document.getElementById('task-input');
+var minuteInput = document.getElementById('minute-input');
+var secondInput = document.getElementById('second-input');
 
-  if (exButton.isActive === true) {
-    exButton.childNodes[1].src = `./assets/${exButton.name}-active.svg`;
-    exButton.classList.add('ex-color');
-    startButton.disabled = false;
-  } else {
-    exButton.childNodes[1].src = `./assets/${exButton.name}.svg`;
-    exButton.classList.remove('ex-color');
-  };
+form.addEventListener('submit',function(event) {
+  event.preventDefault();
 
-  if (medButton.isActive === true) {
-    medButton.childNodes[1].src = `./assets/${medButton.name}-active.svg`;
-    medButton.classList.add('med-color');
-    startButton.disabled = false;
-  } else {
-    medButton.childNodes[1].src = `./assets/${medButton.name}.svg`;
-    medButton.classList.remove('med-color');
-  };
-
+  inputValidation();
 });
 
-newActivitySection.addEventListener('click', function(event) {
-  // function for that start activity button
-  if(event.target.id === startButton.id) {
-    var selectedCategory = 'Study';
-    var descriptionInput = document.querySelector('#task-input').value;
-    var minuteInput = document.querySelector('#minute-input').value;
-    var secondInput = document.querySelector('#second-input').value;
-    var currentActivity = new Activity(selectedCategory, descriptionInput, minuteInput, secondInput);
-    debugger;
-    console.log('test');
+
+function inputValidation() {
+  var taskValue = task.value.trim();
+  var minuteValue = minuteInput.value.trim();
+  var secondValue = secondInput.value.trim();
+
+  if(taskValue === ""){
+    errorMessage(task, '<img class="warning" src="assets/warning.svg"> Please Enter A Task!');
+  } else {
+    success(task);
   }
-});
+}
 
+function errorMessage (input, message){
+  var formError = input.parentElement;
+  var addError = form.querySelector('small');
 
+  addError.innerHTML = message;
+  formError.className = 'form error-message';
+}
 
-
-
-
-//Validation Formulas
-// function numValidator(x) {
-//   var invalidChar = ['e', 'E', '+', '-'];
-//   if (isNaN(x) && invalidChar.includes(x))
-//     //innerHTML to add error message for correct numbers
-//   }
-// }
-//
-// function textValidator() {
-//   if(//input === "") {
-//     //return innerHTML error message
-//   }
-// }
-//
-// function submit() {
-//   event.preventDefault();
-// }
-
-
+function success(input) {
+  var formError = input.parentElement;
+  formError.className = 'form';
+}
 
 //**** studButton comments ****** */
 
@@ -92,7 +125,7 @@ newActivitySection.addEventListener('click', function(event) {
 // )
 // var targetName = event.target.name;
 // event.target.childNodes[1].src = `./assets/${targetName}-active.svg`
-
+//
 
 // console.log("1:", event.target.childNodes[1].src)
 
