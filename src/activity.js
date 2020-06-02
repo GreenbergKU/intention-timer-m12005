826ec72a3fa996ad1c7493f52ba60b355f5a7886
Suzie Q <suzieqwolf@gmail.com>
2020-06-02 17:37:34 -0600

    Adds savedToStorage function, storing pastActivities in LocalStorage.

--- src/activity.js
+++ src/activity.js
@@ -1,27 +1,27 @@
 class Activity {
-    constructor(category, description, minutes, seconds) {
-        this.id = Date.now();
-        this.category = category;
-        this.description = description;
-        this.minutes = minutes;
-        this.seconds = seconds;
-        this.isCompleted = false;
-        this.totalSeconds = this.minutes * 60 + this.seconds;
+  constructor(category, description, minutes, seconds) {
+    this.id = Date.now();
+    this.category = category;
+    this.description = description;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.isCompleted = false;
+    this.totalSeconds = this.minutes * 60 + this.seconds;
+  }
+  startTimer() {
+    this.endTime = Date.now() + this.totalSeconds * 1000;
+  }
 
-     }
+  secondsLeft() {
+    return Math.floor((this.endTime - Date.now()) / 1000);
+  }
 
-     startTimer() {
-       this.endTime = Date.now() + this.totalSeconds * 1000;
-     }
+  markComplete() {
+    this.isCompleted = true;
+  }
 
-     secondsLeft() {
-       return Math.floor((this.endTime - Date.now()) / 1000);
-     }
-
-     markComplete() {
-     this.isCompleted = true;
-     }
-
-     saveToStorage() {
-     }
-   };
+  saveToStorage(array) {
+    var stringifiedActivities = JSON.stringify(array);
+    localStorage.setItem('pastActivities', stringifiedActivities);
+  } 
+};
