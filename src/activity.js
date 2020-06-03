743fac23f6cf927ceb237d2b4806ca2c42917f88
Suzie Q <suzieqwolf@gmail.com>
2020-06-03 16:19:20 -0600

    Adds refactored code, empasis on style, according to Turing's JS style guide.

--- src/activity.js
+++ src/activity.js
@@ -7,22 +7,22 @@ class Activity {
     this.id = id || Date.now();
     this.isCompleted = false;
     this.totalSeconds = this.minutes * 60 + this.seconds;
-  }
+  };
 
   startTimer() {
     this.endTime = Date.now() + this.totalSeconds * 1000;
-  }
+  };
 
   secondsLeft() {
     return Math.floor((this.endTime - Date.now()) / 1000);
-  }
+  };
 
   markComplete() {
     this.isCompleted = true;
-  }
+  };
 
   saveToStorage(array) {
     var stringifiedActivities = JSON.stringify(array);
     localStorage.setItem('pastActivities', stringifiedActivities);
-  }
+  };
 };
